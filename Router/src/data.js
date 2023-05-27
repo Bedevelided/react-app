fb5e68b79166c689445e6f48b70794776141204f
M.camara <58867220+Bedevelided@users.noreply.github.com>
2023-05-27 23:52:44 +0000

    Add files via upload

---
+++ Router/src/data.js
@@ -0,0 +1,44 @@
+
+import movie1 from "./images/movie-2.jpg";
+import movie2 from "./images/movie-3.jpg";
+import movie3 from "./images/movie-4.jpg";
+import movie4 from "./images/movie-5.jpg";
+export const movies = [
+    {
+         title : "Angel of Mine",
+         desc : "Lizzie a perdu sa petite fille il y a des années. Quand elle rencontre une fillette, elle est convaincue que cette dernière est en réalité la sienne...",
+         posteUrl :  movie1,
+         rating : 8,
+         video : "https://www.youtube.com/watch?v=IwL8b4QsSYM",
+         description : "Lizzie a perdu sa petite fille il y a des années. Quand elle rencontre une fillette, elle est convaincue que cette dernière est en réalité la sienne. Elle devient obsédée par l'enfant et perd petit à petit contact avec la réalité..",
+         id: 1
+        },
+    {
+        title : "Top.Gun.Maverick",
+        desc : "Après avoir été l’un des meilleurs pilotes de chasse de la Marine américaine pendant plus de trente ans, Pete 'Maverick' Mitchell continue...",
+        posteUrl : movie2,
+        rating : 7,
+        vidoe : "https://www.youtube.com/watch?v=giXco2jaZ_4",
+        description : "Après avoir été l’un des meilleurs pilotes de chasse de la Marine américaine pendant plus de trente ans, Pete 'Maverick' Mitchell continue à repousser ses limites en tant que pilote d'essai. Il refuse de monter en grade, car cela l’obligerait à renoncer à voler. Il est chargé de former un détachement de jeunes diplômés de l’école Top Gun pour une mission spéciale qu’aucun pilote n'aurait jamais imaginée. Lors de cette mission,",
+        id : 2
+    },
+    {
+        title : "Samaritan",
+        desc : 'Âgé de 13 ans, Sam Cleary suspecte que son mystérieux et solitaire voisin, Mr. Smith, soit en réalité...',
+        posteUrl :  movie3,
+        rating : 8.5,
+        video : "https://www.youtube.com/watch?v=9FKnTxSC16E",
+        description : "Âgé de 13 ans, Sam Cleary suspecte que son mystérieux et solitaire voisin, Mr. Smith, soit en réalité une légende qui se cache au grand jour. Il y a 25 ans, le superpuissant justicier de Granite City, Le Samaritain, a été déclaré mort dans un entrepôt, après un combat tragique avec son rival, Nemesis. La plupart des gens pensent que Le Samaritain a péri dans les flammes, mais certains habitants, comme Sam, ont bon espoir qu’il soit encore en vie",
+        id : 3
+    },
+    {
+        title : "Spider-Man.No.Way.Home",
+        desc : 'Pour la première fois dans son histoire cinématographique, Spider-Man, le héros sympa du quartier est démasqué et ne ...',
+        posteUrl :  movie4,
+        rating : 6,
+        video : "https://www.youtube.com/watch?v=JfVOs4VSpmA",
+        description : "Pour la première fois dans son histoire cinématographique, Spider-Man, le héros sympa du quartier est démasqué et ne peut désormais plus séparer sa vie normale de ses lourdes responsabilités de super-héros. Quand il demande de l'aide à Doctor Strange, les enjeux deviennent encore plus dangereux, le forçant à découvrir ce qu'être Spider-Man signifie véritablement.",
+        id : 4
+       
+    },
+]
